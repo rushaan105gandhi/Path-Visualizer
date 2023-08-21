e3df6e53edef869eb3b70d457b9e627e38167b1c
rushaan gandhi <rushaansps@gmail.com>
2023-08-21 23:03:57 +0530

    initialized nodes

--- path-finder/src/Pathfinding Visualizer/PathfindingVisualizer.jsx
+++ path-finder/src/Pathfinding Visualizer/PathfindingVisualizer.jsx
@@ -13,11 +13,16 @@ export default class PathfindingVisualizer extends Component {
 
     componentDidMount() {
         const nodes = [];
-
         for(let row = 0; row < 20; row++) {
             const currentRow = [];
             for(let col = 0; col < 50; col++) {
-                currentRow.push([]);
+                const currentNode = {
+                    col,
+                    row,
+                    isStart: row === 10 && col === 5,
+                    isFinish: row === 10 && col ===45,
+                };
+                currentRow.push(currentNode);
             }
             nodes.push(currentRow);
         }
@@ -31,10 +36,22 @@ export default class PathfindingVisualizer extends Component {
         return (
             <div className="grid">
                 {nodes.map((row, rowIdx) => {
-                    return <div>
-                        {row.map((node, nodeIdx) => <Node></Node>)}
+                    return (
+                    <div key={rowIdx}>
+                        {row.map((node, nodeIdx) => {
+                            const {isStart, isFinish} = node;
+                            return (
+                                <Node 
+                                key={nodeIdx}
+                                isStart={isStart}
+                                isFinish={isFinish}
+                                test={'foo'}
+                                ></Node>
+                            );
+                        })}
                     </div>
-                })}
+                    );
+                })};
             </div>
         );
     }
