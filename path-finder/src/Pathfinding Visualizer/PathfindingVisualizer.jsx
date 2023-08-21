4a6ad86d93c8020d2bba04608989e8ccf9ce6abc
rushaan gandhi <rushaansps@gmail.com>
2023-08-21 13:48:09 +0530

    grid formation

--- path-finder/src/Pathfinding Visualizer/PathfindingVisualizer.jsx
+++ path-finder/src/Pathfinding Visualizer/PathfindingVisualizer.jsx
@@ -6,14 +6,35 @@ import './PathfindingVisualizer.css';
 export default class PathfindingVisualizer extends Component { 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            nodes: [],
+        };
+    }
+
+    componentDidMount() {
+        const nodes = [];
+
+        for(let row = 0; row < 20; row++) {
+            const currentRow = [];
+            for(let col = 0; col < 50; col++) {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes})
     }
 
     render() {
+        const {nodes} = this.state;
+        console.log(nodes)
+
         return (
-            <div>
-            Foo 
-            <Node></Node>
+            <div className="grid">
+                {nodes.map((row, rowIdx) => {
+                    return <div>
+                        {row.map((node, nodeIdx) => <Node></Node>)}
+                    </div>
+                })}
             </div>
         );
     }
